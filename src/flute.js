82b38154e4a112415f266b720c7ce107d92ed3bd
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2021-10-01 22:31:22 -0500

    Added more test images

--- src/flute.js
+++ src/flute.js
@@ -4,7 +4,10 @@ const mainKey = () => {
     const div = document.createElement("div")
     div.innerHTML = `
         <svg height="100" wdith="100">
-            <circle cx="50" cy="50" r="40" stroke="black" stroke-width="3" fill="red" />
+            <circle cx="50" cy="50" r="40" stroke="silver" stroke-width="3" fill="black" />
+        </svg>
+        <svg height="50" wdith="50">
+            <circle cx="25" cy="25" r="20" stroke="silver" stroke-width="3" fill="none" />
         </svg>
     `
     chart.appendChild(div)
