a71960b48150792cd712167f9d3aa837bf1ef872
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2021-10-02 22:02:04 -0500

    Added keys to html and added starter code for choosing keys

--- src/index.js
+++ src/index.js
@@ -1,7 +1,23 @@
+const keyItems = document.getElementsByClassName('key-item')
+
 // Set up functions
 const init = () => {
     console.log('Start fingerings app')
+    setKeyPress()
     mainKey()
+}
+
+const setKeyPress = () => {
+    console.log('setKeyPress Function')
+    Array.from(keyItems).forEach(element => {
+        element.addEventListener('click', keyPress)
+    })
+}
+
+const keyPress = () => {
+    console.log('keyPress Function')
+    console.log('Add code to fill/clear svgs by id')
+    console.log(event.target)
 }
 
 // Start app
