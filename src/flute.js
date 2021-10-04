3f9c107ad1678a55f40fd6454cda2c17de4c2939
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2021-10-04 18:19:28 -0500

    Fixed spelling error on SVG so width is properly implemented

--- src/flute.js
+++ src/flute.js
@@ -1,17 +1,17 @@
 const keysFlute = () => {
     const keySetup = `
         <div id="key-lh1" class="key-item">
-            <svg height="40" wdith="40">
+            <svg height="40" width="40">
                 <circle cx="20" cy="20" r="15" stroke="silver" stroke-width="3" fill="red" />
             </svg>
         </div>
         <div id="key-lh2" class="key-item">
-            <svg height="40" wdith="40">
+            <svg height="40" width="40">
                 <circle cx="20" cy="20" r="15" stroke="silver" stroke-width="3" fill="orange" />
             </svg>
         </div>
         <div id="key-lh3" class="key-item">
-            <svg height="40" wdith="40">
+            <svg height="40" width="40">
                 <circle cx="20" cy="20" r="15" stroke="silver" stroke-width="3" fill="yellow" />
             </svg>
         </div>
