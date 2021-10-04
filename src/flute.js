38f6c6de4d775322a1b7bb23503ec81bafde2ef5
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2021-10-04 18:04:44 -0500

    Adjusted to a smaller SVG size for primary keys

--- src/flute.js
+++ src/flute.js
@@ -5,18 +5,18 @@ const keysFlute = () => {
     div.innerHTML = `
     <div>
         <div id="key-lh1" class="key-item">
-            <svg height="100" wdith="100">
-                <circle cx="50" cy="50" r="40" stroke="silver" stroke-width="3" fill="red" />
+            <svg height="30" wdith="30">
+                <circle cx="15" cy="15" r="10" stroke="silver" stroke-width="3" fill="red" />
             </svg>
         </div>
         <div id="key-lh2" class="key-item">
-            <svg height="100" wdith="100">
-                <circle cx="50" cy="50" r="40" stroke="silver" stroke-width="3" fill="orange" />
+            <svg height="30" wdith="30">
+                <circle cx="15" cy="15" r="10" stroke="silver" stroke-width="3" fill="orange" />
             </svg>
         </div>
         <div id="key-lh3" class="key-item">
-            <svg height="100" wdith="100">
-                <circle cx="50" cy="50" r="40" stroke="silver" stroke-width="3" fill="yellow" />
+            <svg height="30" wdith="30">
+                <circle cx="15" cy="15" r="10" stroke="silver" stroke-width="3" fill="yellow" />
             </svg>
         </div>
     </div>
