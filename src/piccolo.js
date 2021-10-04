b7c9c9be2ae3125c6e778c2da5c07b9cef4d621c
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2021-10-04 18:05:47 -0500

    Added piccolo js file with current sizes

---
+++ src/piccolo.js
@@ -0,0 +1,25 @@
+const chart = document.getElementById("pane-two-top")
+
+const keysFlute = () => {
+    const div = document.createElement("div")
+    div.innerHTML = `
+    <div>
+        <div id="key-lh1" class="key-item">
+            <svg height="30" wdith="30">
+                <circle cx="15" cy="15" r="10" stroke="silver" stroke-width="3" fill="red" />
+            </svg>
+        </div>
+        <div id="key-lh2" class="key-item">
+            <svg height="30" wdith="30">
+                <circle cx="15" cy="15" r="10" stroke="silver" stroke-width="3" fill="orange" />
+            </svg>
+        </div>
+        <div id="key-lh3" class="key-item">
+            <svg height="30" wdith="30">
+                <circle cx="15" cy="15" r="10" stroke="silver" stroke-width="3" fill="yellow" />
+            </svg>
+        </div>
+    </div>
+    `
+    chart.appendChild(div)
+}
