54cbccf722e832cc41ac4f0baccd92ea9591654f
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2021-10-06 20:39:31 -0500

    Added finger names to diagram

--- src/flute.js
+++ src/flute.js
@@ -1,31 +1,37 @@
 const keysFlute = () => {
     const keySetup = `
         <div id="key-lh-1" class="key-item">
+            <p class="key-name">LH 1</p>
             <svg height="40" width="40">
                 <circle cx="20" cy="20" r="15" stroke="silver" stroke-width="3" fill="none" />
             </svg>
         </div>
         <div id="key-lh-2" class="key-item">
+            <p class="key-name">LH 2</p>
             <svg height="40" width="40">
                 <circle cx="20" cy="20" r="15" stroke="silver" stroke-width="3" fill="none" />
             </svg>
         </div>
         <div id="key-lh-3" class="key-item">
+            <p class="key-name">LH 3</p>
             <svg height="40" width="40">
                 <circle cx="20" cy="20" r="15" stroke="silver" stroke-width="3" fill="none" />
             </svg>
         </div>
         <div id="key-rh-1" class="key-item">
+            <p class="key-name">RH 1</p>
             <svg height="40" width="40">
                 <circle cx="20" cy="20" r="15" stroke="silver" stroke-width="3" fill="none" />
             </svg>
         </div>
         <div id="key-rh-2" class="key-item">
+            <p class="key-name">RH 2</p>
             <svg height="40" width="40">
                 <circle cx="20" cy="20" r="15" stroke="silver" stroke-width="3" fill="none" />
             </svg>
         </div>
         <div id="key-rh-3" class="key-item">
+            <p class="key-name">RH 3</p>
             <svg height="40" width="40">
                 <circle cx="20" cy="20" r="15" stroke="silver" stroke-width="3" fill="none" />
             </svg>
