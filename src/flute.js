a6e6e95e3d7852abb73f9f4952fb5e220898fb2a
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2021-10-07 14:18:57 -0500

    Added first trill key

--- src/flute.js
+++ src/flute.js
@@ -24,6 +24,13 @@ const keysFlute = () => {
                 <circle cx="20" cy="20" r="15" stroke="silver" stroke-width="3" fill="none" />
             </svg>
         </div>
+        <div id="key-rh-tr1" class="key-item key-lower">
+            <p class="key-name-lower">TR 1</p>
+            <svg height="63" width="18">
+            <ellipse cx="9" cy="24" rx="5" ry="12" stroke="silver" stroke-width="2" fill="none" />
+            Sorry, your browser does not support inline SVG.  
+            </svg>
+        </div>
         <div id="key-rh-2" class="key-item">
             <p class="key-name">RH 2</p>
             <svg height="40" width="40">
