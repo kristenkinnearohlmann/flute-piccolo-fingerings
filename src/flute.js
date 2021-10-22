97e169c4cd3829862d1ebcc70cd02a5a1ff78d77
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2021-10-21 21:39:48 -0500

    Changed thumb key to rectangle, removed key names to relocate later

--- src/flute.js
+++ src/flute.js
@@ -1,77 +1,66 @@
 const keysFlute = () => {
     const keySetup = `
-        <div id="key-th-bflat" class="key-item">
-            <p class="key-name-lower">Th B fl</p>
-            <svg height="63" width="8" style="border: 1px solid blue;">
-                <path d="M 1 50 h 6 v 12 h -6 v -12" stroke="silver" stroke-width="2" fill="none" />
-            Sorry, your browser does not support inline SVG.
+        <div id="key-th-bflat" class="key-item key-lower">
+            <svg height="63" width="18">
+                <rect x="6" y="25" height="10" width="10" stroke="silver" stroke-width="2" fill="none" />
+                Sorry, your browser does not support inline SVG.  
             </svg>
         </div>
         <div id="key-th" class="key-item">
-            <p class="key-name-lower">Th</p>
             <svg height="63" width="30">
                 <path d="M 10 50 h 18 v 12 h -18 v -12" stroke="silver" stroke-width="2" fill="none" />
                 Sorry, your browser does not support inline SVG.
             </svg>
         </div>
         <div id="key-lh-1" class="key-item">
-            <p class="key-name">LH 1</p>
             <svg height="40" width="40">
                 <circle cx="20" cy="20" r="15" stroke="silver" stroke-width="3" fill="none" />
                 Sorry, your browser does not support inline SVG.
             </svg>
         </div>
         <div id="key-lh-2" class="key-item">
-            <p class="key-name">LH 2</p>
             <svg height="40" width="40">
                 <circle cx="20" cy="20" r="15" stroke="silver" stroke-width="3" fill="none" />
                 Sorry, your browser does not support inline SVG.
             </svg>
         </div>
         <div id="key-lh-3" class="key-item">
-            <p class="key-name">LH 3</p>
             <svg height="40" width="40">
                 <circle cx="20" cy="20" r="15" stroke="silver" stroke-width="3" fill="none" />
                 Sorry, your browser does not support inline SVG.
             </svg>
         </div>
         <div id="key-lh-4" class="key-item">
-            <p class="key-name">LH 4</p>
             <svg height="60" width="40">
             <path d="M 10 10 h 20 v 40 h -5 v -28 h -15 v -12" stroke="silver" stroke-width="3" fill="none" />
             Sorry, your browser does not support inline SVG.
             </svg>
         </div>
         <div id="key-rh-1" class="key-item">
-            <p class="key-name">RH 1</p>
             <svg height="40" width="40">
                 <circle cx="20" cy="20" r="15" stroke="silver" stroke-width="3" fill="none" />
                 Sorry, your browser does not support inline SVG.
             </svg>
         </div>
         <div id="key-rh-tr1" class="key-item key-lower">
-            <p class="key-name-lower">TR 1</p>
             <svg height="63" width="18">
             <ellipse cx="9" cy="24" rx="5" ry="12" stroke="silver" stroke-width="2" fill="none" />
             Sorry, your browser does not support inline SVG.
             </svg>
         </div>
         <div id="key-rh-2" class="key-item">
-            <p class="key-name">RH 2</p>
             <svg height="40" width="40">
                 <circle cx="20" cy="20" r="15" stroke="silver" stroke-width="3" fill="none" />
                 Sorry, your browser does not support inline SVG.
             </svg>
         </div>
         <div id="key-rh-tr2" class="key-item key-lower">
-            <p class="key-name-lower">TR 2</p>
             <svg height="63" width="18">
             <ellipse cx="9" cy="24" rx="5" ry="12" stroke="silver" stroke-width="2" fill="none" />
             Sorry, your browser does not support inline SVG.
             </svg>
         </div>
         <div id="key-rh-3" class="key-item">
-            <p class="key-name">RH 3</p>
             <svg height="40" width="40">
                 <circle cx="20" cy="20" r="15" stroke="silver" stroke-width="3" fill="none" />
                 Sorry, your browser does not support inline SVG.
