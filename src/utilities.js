c241087e073f8682a1e3faca93bd30f7d2ea1d6c
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2022-08-28 23:07:23 -0500

    Moved number range generator to utilities

--- src/utilities.js
+++ src/utilities.js
@@ -1,3 +1,10 @@
+// functional programming version of range for JS
+// https://dev.to/ycmjason/how-to-create-range-in-javascript-539i
+const generateNumberRange = (start, end) => {
+  if (start === end) return [start];
+  return [start, ...generateNumberRange(start + 1, end)];
+};
+
 const populateDropdownList = () => {
   console.log("Utilities run here");
 };
