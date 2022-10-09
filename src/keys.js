3276543f984596181c5cf51710a6ac84ab4e2f96
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2022-10-09 16:43:37 -0500

    Prep explanation for piccolo octaves

--- src/keys.js
+++ src/keys.js
@@ -1,7 +1,11 @@
+const explanationPane = document.getElementById("note-finger-explanation");
+
 const renderKeys = (instrument, screenSize) => {
   switch (instrument) {
     case "flute":
+      explanationPane.innerText = "Pane 2: Explanation";
     case "piccolo":
+      explanationPane.innerText = "Sounds 1 octave higher than written";
       renderFlutePiccoloKeys(instrument, screenSize);
       break;
     default:
