10ba65c371a2acc85587b6ffa74d03c3b2984b91
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2022-11-27 18:56:06 -0600

    Add draft key set

--- src/keys.js
+++ src/keys.js
@@ -1,10 +1,32 @@
 const explanationPane = document.getElementById("note-finger-explanation");
+// options
+// th1: key-th-bflat
+// th2: key-th
+// l1: key-lh-1
+// l2: key-lh-2
+// l3: key-lh-3
+// pl1: key-lh-4
+// r1: key-rh-1
+// tr1: key-rh-tr1
+// r2: key-rh-2
+// tr2: key-rh-tr2
+// r3: key-rh-3
+// pr1: key-rh-pinky
+// r1: key-rh-broll
+// r2: key-rh-croll
+// pr2: key-rh-csharp
 const fluteKeys = {
   3: {},
   4: {
     1: [],
     2: [],
-    3: [{ title: "Basic", description: "", keys: [] }],
+    3: [
+      {
+        title: "Basic",
+        description: "",
+        keys: setKeys(["th2", "l2", "l3", "r1", "r2", "r3"]),
+      },
+    ],
     4: [],
     5: [],
     6: [],
